961e686916819714a9b2fe443b13a87825b787e5
bakerjaid <jaidencolebaker@gmail.com>
2025-12-23 10:12:54 +1300

    Cleanup

--- src/main/java/com/jdz/noae/mixins/EarlyMixinsLoader.java
+++ src/main/java/com/jdz/noae/mixins/EarlyMixinsLoader.java
@@ -7,7 +7,6 @@ import java.util.Set;
 
 import com.gtnewhorizon.gtnhmixins.IEarlyMixinLoader;
 
-import cpw.mods.fml.relauncher.FMLLaunchHandler;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 
 @IFMLLoadingPlugin.MCVersion("1.7.10")
@@ -43,34 +42,6 @@ public class EarlyMixinsLoader implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
     @Override
     public List<String> getMixins(Set<String> loadedCoreMods) {
-        // Register your mixins here by adding them to the list.
-        // The early mixins are mixins that target minecraft/forge classes
-        // The early mixins should be placed in the "mixins/early" package
-        // If your mixins targets a class from another mod, register it in the LateMixinLoader
-        List<String> mixins = new ArrayList<>();
-
-        // The parameter loadedCoreMods contains the name of coremods that are currently loaded
-        // you can check this Set to decide to load certain mixins or not.
-        // if (!loadedCoreMods.contains("optifine.OptiFineForgeTweaker")) {
-        // // this mixins won't be loaded if Optifine is present
-        // mixins.add("MixinClass");
-        // }
-
-        if (FMLLaunchHandler.side()
-            .isClient()) {
-            // register here your mixins that should only be loaded on the client
-            // mixins.add("MixinMinecraft_Example");// this is an example you should delete it and the associated mixin
-            // class as well
-        } else {
-            // register here your mixins that should only be loaded on the dedicated server
-            // mixins.add("MixinClass");
-        }
-
-        // register here your mixins that should be loaded on both sides
-        // mixins.add("MixinClass");
-
-        // If you need more complex registration logic consider switching to the IMixins registration
-        // system see com.gtnewhorizon.gtnhmixins.builders.IMixins
-        return mixins;
+        return new ArrayList<>();
     }
 }
