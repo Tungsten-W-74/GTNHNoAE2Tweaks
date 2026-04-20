4108883bd51349c394b6aad090c0fda2b0c78fbb
bakerjaid <jaidencolebaker@gmail.com>
2026-04-21 11:24:45 +1200

    Spotless

--- src/main/java/com/jdz/noae/mixins/late/RecipeLock/RecipeLockDTPF.java
+++ src/main/java/com/jdz/noae/mixins/late/RecipeLock/RecipeLockDTPF.java
@@ -1,8 +1,8 @@
 package com.jdz.noae.mixins.late.RecipeLock;
 
-import gregtech.common.tileentities.machines.multi.MTEPlasmaForge;
-import gtPlusPlus.xmod.gregtech.common.tileentities.machines.multi.processing.MTEIndustrialAlloySmelter;
 import org.spongepowered.asm.mixin.Mixin;
+
+import gregtech.common.tileentities.machines.multi.MTEPlasmaForge;
 
 @Mixin(MTEPlasmaForge.class)
 public class RecipeLockDTPF {
